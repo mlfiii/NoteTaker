2aeed544d7c60513bd24b3a6017ec9e4c52f8322
Vero Verto <gin@luthercorp.com>
2020-03-31 19:42:09 -0700

    Made some adjustments.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -26,4 +26,9 @@ module.exports = function (app) {
   app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
+
+  // If no matching route is found default to home
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
 };
